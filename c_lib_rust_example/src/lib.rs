46e941981505c13970546998204e9cce6525bf3e
Tony Asleson <tony.asleson@gmail.com>
2025-03-19 08:40:26 -0500

    Make the code work with 2024 edition
    Provided by https://github.com/ostkamp
    ref. https://github.com/tasleson/crispy-memory/issues/1
    
    Signed-off-by: Tony Asleson <tony.asleson@gmail.com>

--- c_lib_rust_example/src/lib.rs
+++ c_lib_rust_example/src/lib.rs
@@ -5,52 +5,56 @@ use std::os::raw::c_char;
 /// File: lib.rs
 
 /// For further reading ...
-/// #[no_mangle] - // https://internals.rust-lang.org/t/precise-semantics-of-no-mangle/4098
+/// #[unsafe(no_mangle)] - // https://internals.rust-lang.org/t/precise-semantics-of-no-mangle/4098
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn get_some_cstr(desc: *mut *mut c_char) -> isize {
-    // We want the pointer coming in to not be null and not currently be pointing to something
-    // to prevent whatever it's pointing to be lost.
-    if desc.is_null() || !(*desc).is_null() {
-        return libc::EINVAL as isize;
+    unsafe {
+        // We want the pointer coming in to not be null and not currently be pointing to something
+        // to prevent whatever it's pointing to be lost.
+        if desc.is_null() || !(*desc).is_null() {
+            return libc::EINVAL as isize;
+        }
+
+        let val = CString::new("Returning a string to C to be free() there")
+            .expect("Expecting we can allocate a CString");
+
+        // Allocate memory for string as C caller is expected to "free" it.
+        // This approach seems to be the safest way to do this, so that you can be certain
+        // that the memory is allocated with the same allocator as what the caller will be using to
+        // "free" it.  In general having a library which allocates things on the heap and expects the
+        // caller to free it is probably not the best thing to do.
+        let m = libc::malloc(libc::strlen(val.as_ptr()) + 1) as *mut c_char;
+        if m.is_null() {
+            return libc::ENOMEM as isize;
+        }
+
+        *desc = m;
+        libc::strcpy(*desc, val.as_ptr());
+        0
     }
-
-    let val = CString::new("Returning a string to C to be free() there")
-        .expect("Expecting we can allocate a CString");
-
-    // Allocate memory for string as C caller is expected to "free" it.
-    // This approach seems to be the safest way to do this, so that you can be certain
-    // that the memory is allocated with the same allocator as what the caller will be using to
-    // "free" it.  In general having a library which allocates things on the heap and expects the
-    // caller to free it is probably not the best thing to do.
-    let m = libc::malloc(libc::strlen(val.as_ptr()) + 1) as *mut c_char;
-    if m.is_null() {
-        return libc::ENOMEM as isize;
-    }
-
-    *desc = m;
-    libc::strcpy(*desc, val.as_ptr());
-    0
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn get_some_cstr_2(desc: *mut *mut c_char) -> isize {
-    // We want the pointer coming in to not be null and not currently be pointing to something
-    // to prevent whatever it's pointing to be lost.
-    if desc.is_null() || !(*desc).is_null() {
-        return libc::EINVAL as isize;
+    unsafe {
+        // We want the pointer coming in to not be null and not currently be pointing to something
+        // to prevent whatever it's pointing to be lost.
+        if desc.is_null() || !(*desc).is_null() {
+            return libc::EINVAL as isize;
+        }
+
+        let val = CString::new("Returning a string to C to be free() there")
+            .expect("Expecting we can allocate a CString");
+
+        // The documentation states that the pointer from "into_raw()"
+        // needs to be brought back into rust and reconstructed as a CString to be freed
+        // correctly, so this example is suppose to result into a memory leak if the memory
+        // is released with free().  This is stated because you need to ensure the allocator &
+        // de-allocator are the same.
+        *desc = val.into_raw();
+        0
     }
-
-    let val = CString::new("Returning a string to C to be free() there")
-        .expect("Expecting we can allocate a CString");
-
-    // The documentation states that the pointer from "into_raw()"
-    // needs to be brought back into rust and reconstructed as a CString to be freed
-    // correctly, so this example is suppose to result into a memory leak if the memory
-    // is released with free().  This is stated because you need to ensure the allocator &
-    // de-allocator are the same.
-    *desc = val.into_raw();
-    0
 }
 
 /// Our implementation of the Error type.
@@ -80,51 +84,55 @@ impl Drop for Error {
     }
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn error_create_with_result(_e: *mut *mut Error) -> isize {
-    let e = Box::new(example_error());
-    *_e = Box::into_raw(e);
-    0
+    unsafe {
+        let e = Box::new(example_error());
+        *_e = Box::into_raw(e);
+        0
+    }
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn error_free_with_result(e: *mut *mut Error) -> i32 {
-    if e.is_null() || (*e).is_null() {
-        return libc::EINVAL;
-    }
+    unsafe {
+        if e.is_null() || (*e).is_null() {
+            return libc::EINVAL;
+        }
 
-    // Try to ensure we have a pointer to the correct structure...
-    if (*(*e)).magic != ERROR_MAGIC {
-        return libc::EINVAL;
-    }
+        // Try to ensure we have a pointer to the correct structure...
+        if (*(*e)).magic != ERROR_MAGIC {
+            return libc::EINVAL;
+        }
 
-    // Reconstruct the Error into a box and then drop it so that it's freed.
-    drop(Box::from_raw(*e));
-    *e = 0 as *mut Error;
-    0
+        // Reconstruct the Error into a box and then drop it so that it's freed.
+        drop(Box::from_raw(*e));
+        *e = 0 as *mut Error;
+        0
+    }
 }
 
 /// The next two function examples are taken directly out of the rust documentation
 /// ref. https://doc.rust-lang.org/std/boxed/
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn error_new() -> Box<Error> {
     Box::new(example_error())
 }
 
 /// We take ownership as we are passing by value, so when function
 /// exits the drop gets run.  Handles being passed null.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn error_free(_: Option<Box<Error>>) {}
 
 /// Our example "getter" methods which work on the Error type.  The value
 /// returned is only valid as long as the Error has not been freed.  If C
 /// caller needs a longer lifetime they need to copy the value.
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn error_msg_get(e: &Error) -> *const c_char {
     e.msg.as_ptr()
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn error_code_get(e: &Error) -> isize {
     e.code
 }
